80db0f6954fc0d0806e9fd79fbfab95359ce0e46
Николай Зернов <112773094+WayHacker@users.noreply.github.com>
2023-05-28 18:16:44 +0300

    Add files via upload

---
+++ lab3_b/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <vector>
+#include <iostream>
+
+class Detail {
+protected:
+	Detail() { std::cout << "This is class Detail" << std::endl; }
+public:
+	template <typename T> 
+	friend void add_vector(std::vector<Detail*>& v);
+
+};
+
+class Assembling : public Detail {
+protected:
+	Assembling() { std::cout << "This is Assembling" << std::endl; }
+public:
+	template <typename T> 
+	friend void add_vector(std::vector<Detail*>& v);
+};
+
+template <typename T> void add_vector(std::vector <Detail*>& v)
+{
+	v.push_back(new T);
+}
+
+int main() {
+	std::vector <Detail*> v1;
+	add_vector<Assembling>(v1);
+	add_vector<Detail>(v1);
+	return 0;
+
+}
